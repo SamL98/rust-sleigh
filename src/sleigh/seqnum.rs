a8377f0cea64489a9a7cf9820673f5cb402d4141
Sam Lerner <lerner98@gmail.com>
2025-01-22 19:57:59 -0500

    added debug and display to seqnum

--- src/sleigh/seqnum.rs
+++ src/sleigh/seqnum.rs
@@ -1,5 +1,7 @@
 use super::types::SeqNum;
 
+use std::fmt;
+
 impl SeqNum {
     pub fn next(&self) -> Self {
         Self {
@@ -15,5 +17,21 @@ impl SeqNum {
             uniq: self.uniq - 1,
             order: self.order,
         }
+    }
+
+    fn to_string(&self) -> String {
+        format!("{}:{}", self.pc, self.uniq)
+    }
+}
+
+impl fmt::Debug for SeqNum {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.to_string())
+    }
+}
+
+impl fmt::Display for SeqNum {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.to_string())
     }
 }
