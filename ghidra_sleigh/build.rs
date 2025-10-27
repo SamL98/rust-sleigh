59712be4d68a256448185a4e44a9ccc3c6105fdd
Sam Lerner <lerner98@gmail.com>
2025-10-27 19:34:03 -0400

    removed old tests

--- ghidra_sleigh/build.rs
+++
@@ -1,8 +0,0 @@
-extern crate cmake;
-use cmake::Config;
-
-fn main() {
-    let dst = Config::new("libcsleigh").build();       
-    println!("cargo:rustc-link-search=native={}", dst.display());
-    println!("cargo:rustc-link-lib=dylib=csleigh");
-}
